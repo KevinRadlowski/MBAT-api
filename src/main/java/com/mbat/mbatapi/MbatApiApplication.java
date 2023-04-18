cb15f4b1bda1e9bc0e2d43020f399b2c0efcfe28
MaidenQuinn <kevin.radlowski@gmail.com>
2023-04-18 20:57:23 +0200

    mise en place de security et d'un systeme de login/register fonctionnel

---
+++ src/main/java/com/mbat/mbatapi/MbatApiApplication.java
@@ -0,0 +1,13 @@
+package com.mbat.mbatapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbatApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbatApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mbat/mbatapi/MbatApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbat.mbatapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbatApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
